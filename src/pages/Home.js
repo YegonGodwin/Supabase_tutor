05dc5ad3ecc1494344d15fa76160d6875aa78230
YegonGodwin <godwink685@gmail.com>
2025-03-14 21:08:06 +0300

    Supabase features added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,40 @@
+import supabase from "../Supabase/clients"
+import { useState, useEffect } from "react"
+
 const Home = () => {
+  const [fetchError, setFectchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(()=> {
+    const fetchSmoothies = async () => {
+      const {data, error} = await supabase
+        .from('Supa_smoothies')
+        .select()
+
+        if(error){
+          setFectchError("Could not fetch the smoothies")
+          setSmoothies(null)
+          console.log(error)
+        }
+
+        if (data){
+          setSmoothies(data)
+          setFectchError(null)
+        }
+    }
+    fetchSmoothies()
+  }, [])
+
   return (
     <div className="page home">
+      {fetchError && (<p>{fetchError}</p>)}
+      {smoothies &&(
+        <div className="smoothies">
+          {smoothies.map(smoothie => (
+            <p>{smoothie.title}</p>
+          ))}
+        </div>
+      )}
       <h2>Home</h2>
     </div>
   )
