82522aa2eac66a3ee84611f415f0643d1ed06d83
YegonGodwin <godwink685@gmail.com>
2025-03-25 21:20:50 +0300

    Fetchind data code implementation

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,19 +1,18 @@
-import '../index.css'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 const SmoothieCard = ({ smoothie }) => {
-    return (
-        <div>
-            <h3 className='smoothie-card'>{smoothie.title}</h3>
-            <p>{smoothie.method}</p>
-            <div className='rating'>{smoothie.rating}</div>
-            <div className='buttons'>
-                <Link to={`/${smoothie.id}`} className='button'>
-                <i className='material-icons'>Edit</i></Link>
-                <Link to={`/details/${smoothie.id}`} className='button'>Details</Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className="smoothie-card">
+      <h3>{smoothie.title}</h3>
+      <p>{smoothie.method}</p>
+      <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={"/" + smoothie.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
-export default SmoothieCard;
+export default SmoothieCard
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,4 +1,73 @@
 import { useEffect, useState } from 'react'
+import  { useParams } from 'react-router-dom'
+import { supabase } from '../Supabase/clients'
+import { useNavigate } from 'react-router-dom'
+
+const Update = () => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState('')
+
+  useEffect(() => {
+    const fetchSmoothie = async () => {
+      const { data, error } = await supabase
+        .from('Supasmooth')
+        .select()
+        .eq('id', id)
+        .single()
+
+        if(error){
+          navigate('/', {replace: true})
+        }
+        if(data){
+          setTitle(data.title)
+          setMethod(data.method)
+          setRating(data.rating)
+          console.log(data)
+        }
+    }
+    fetchSmoothie()
+  }, [id, navigate])
+  return (
+    <div className="page update">
+      <form>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <textarea
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Create Smoothie Recipe</button>
+
+        {/*formError && <p className="error">{formError}</p>*/}
+      </form>
+    </div>
+  )
+}
+
+export default Update
+
+/*import { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { supabase } from '../supabaseClient'
 
@@ -37,4 +106,4 @@ const Update = () => {
   )
 }
 
-export default Update
+export default Update*/
